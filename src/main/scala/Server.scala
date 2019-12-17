5c359e746aed2943be1a02f28c35d32d75e14e7d
EvanGertis <physevangertis@gmail.com>
2019-12-17 12:59:24 -0500

    finished

---
+++ src/main/scala/Server.scala
@@ -0,0 +1,33 @@
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.Await
+import scala.util.{Failure, Success}
+
+object Server extends App {
+  val host = "0.0.0.0"
+  val port = 9000
+
+  implicit val system: ActorSystem = ActorSystem(name = "helloworld")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  import system.dispatcher
+
+  import akka.http.scaladsl.server.Directives._
+  def route = path("hello") {
+    get {
+      complete("Hello, World!")
+    }
+  }
+
+  val binding = Http().bindAndHandle(route, host, port)
+  binding.onComplete {
+    case Success(_) => println("Success!")
+    case Failure(error) => println(s"Failed: ${error.getMessage}")
+  }
+
+  import scala.concurrent.duration._
+  Await.result(binding, 3.seconds)
+
+}
